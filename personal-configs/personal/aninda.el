b23bfc98b1aa66dae576034319fc769ee09e3588
Aninda Kundu <spacefugitive@yahoo.com>
2013-11-15 19:21:46 +0530

    turning down font size for aninda

--- personal-configs/personal/aninda.el
+++ personal-configs/personal/aninda.el
@@ -9,6 +9,10 @@
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "M-;") 'comment-or-uncomment-line-or-region)
 
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 130)
+
 (provide 'aninda)
 
 ;;; aninda.el ends here
