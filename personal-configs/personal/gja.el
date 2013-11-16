447628d8837d77d15cf84cefff266a3b7cb778ec
Tejas Dinkar <tejas@gja.in>
2013-11-17 00:52:20 +0530

    Commiting my riddance of highlight

--- personal-configs/personal/gja.el
+++ personal-configs/personal/gja.el
@@ -8,14 +8,12 @@
 (setq whitespace-line-column 1000)
 
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+(global-hl-line-mode -1)
 
 (add-hook 'ruby-mode-hook 'turn-on-visual-line-mode)
 
 (add-hook 'LaTeX-mode-hook 'turn-off-auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-visual-line-mode)
-
-(prelude-ensure-module-deps '(solarized-theme))
-(load-theme 'solarized-dark t)
 
 (provide 'gja)
 ;;; gja.el ends here
