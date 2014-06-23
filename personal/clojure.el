c42ba5fe8e0d8b7d3ae1e960b0c79eb54878b3bc
Tejas Dinkar <tejas@gja.in>
2014-06-23 11:30:30 +0530

    Also adding Alex's shortcut for cljr

--- personal/clojure.el
+++ personal/clojure.el
@@ -4,7 +4,9 @@
 
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")))
+                               (cljr-add-keybindings-with-prefix "C-c C-m")
+                               (cljr-add-keybindings-with-prefix "C-c C-x")))
+
 
 (prelude-ensure-module-deps '(cider))
 (setq cider-repl-wrap-history t)
