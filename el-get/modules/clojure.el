6f111374aae40bff19269675a4b81261192af713
Ravi Chandra Padmala <neena@nilenso.com>
2014-09-09 19:21:46 +0530

    Add cider-nrepl history and autosave on running tests

--- el-get/modules/clojure.el
+++ el-get/modules/clojure.el
@@ -1,7 +1,16 @@
 (nel:require-packages 'cider 'clj-refactor 'align-cljlet 'smartparens)
 
+(setq cider-repl-history-file "~/.emacs.d/nrepl-history")
+(setq cider-auto-select-error-buffer t)
+(setq cider-repl-popup-stacktraces t)
+
 (nel:run-after-initializing
   (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
   (add-hook 'clojure-mode-hook 'clj-refactor-mode)
   (add-hook 'clojure-mode-hook 'show-paren-mode)
-  (add-hook 'clojure-mode-hook 'sp-use-paredit-bindings))
+  (add-hook 'clojure-mode-hook 'sp-use-paredit-bindings)
+  (defadvice clojure-test-run-tests (before save-first activate)
+    (save-buffer))
+
+  (defadvice cider-load-current-buffer (before save-first activate)
+    (save-buffer)))
