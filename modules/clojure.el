e839d2ba3e6b80606769615a32dfecd43ecfaf6c
Timothy Andrew <mail@timothyandrew.net>
2014-09-10 14:54:27 +0530

    Remove the `show-smartparens-global-mode` hook from `clojure.el`

--- modules/clojure.el
+++ modules/clojure.el
@@ -10,7 +10,6 @@
   (add-hook 'clojure-mode-hook 'show-paren-mode)
   (add-hook 'clojure-mode-hook 'sp-use-paredit-bindings)
   (add-hook 'clojure-mode-hook (lambda () (cljr-add-keybindings-with-prefix "C-c C-m")))
-  (add-hook 'clojure-mode-hook (show-smartparens-global-mode +1))
   
   (defadvice clojure-test-run-tests (before save-first activate)
     (save-buffer))
