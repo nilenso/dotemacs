20e1264007e74c3c58deefaf64df38bc17a4acbe
Timothy Andrew <mail@timothyandrew.net>
2014-09-17 11:29:30 +0530

    Add modules for sanityinc-solarized.

---
+++ modules/sanityinc-solarized-light.el
@@ -0,0 +1,5 @@
+(nel:require-packages 'sanityinc-solarized)
+
+(nel:run-after-initializing
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/el-get/sanityinc-solarized")
+  (load-theme 'sanityinc-solarized-light t))
