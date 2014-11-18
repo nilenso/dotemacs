3ede56b03ca619cd361d0fd2b3d1b0c3cef5836f
Timothy Andrew <mail@timothyandrew.net>
2014-11-17 21:44:31 -0800

    Enable smartparens for Elixir

--- modules/elixir.el
+++ modules/elixir.el
@@ -2,3 +2,7 @@
 
 (setq elixir-iex-command "/usr/local/bin/iex")
 (setq elixir-compiler-command "/usr/local/bin/elixirc")
+
+(nel:run-after-initializing
+  (add-hook 'elixir-mode-hook 'smartparens-strict-mode)
+  (add-hook 'elixir-mode-hook 'sp-use-paredit-bindings))
