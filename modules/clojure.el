0f39425e24acf6f73c82337cde0e0a60ca853dc7
Tejas Dinkar <tejas@gja.in>
2014-11-28 09:41:25 +0530

    Enabling cider tests, finally

--- modules/clojure.el
+++ modules/clojure.el
@@ -3,6 +3,11 @@
 (setq cider-repl-history-file "~/.emacs.d/nrepl-history")
 (setq cider-auto-select-error-buffer t)
 (setq cider-repl-popup-stacktraces t)
+(setq auto-enable-clojure-test-mode nil)
+
+(defun custom-cider-shortcuts ()
+  (local-set-key (kbd "C-c ,") 'cider-test-run-tests)
+  (local-set-key (kbd "C-c ,") 'cider-test-run-tests))
 
 (nel:run-after-initializing
   (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
@@ -10,12 +15,14 @@
   (add-hook 'clojure-mode-hook 'show-paren-mode)
   (add-hook 'clojure-mode-hook 'sp-use-paredit-bindings)
   (add-hook 'clojure-mode-hook (lambda () (cljr-add-keybindings-with-prefix "C-c C-m")))
+  (add-hook 'clojure-mode-hook 'custom-cider-shortcuts)
 
   (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
   (add-hook 'cider-repl-mode-hook 'show-paren-mode)
   (add-hook 'cider-repl-mode-hook 'sp-use-paredit-bindings)
 
-  (defadvice clojure-test-run-tests (before save-first activate)
+
+  (defadvice cider-test-run-tests (before save-first activate)
     (save-buffer))
 
   (defadvice cider-load-current-buffer (before save-first activate)
