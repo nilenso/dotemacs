6deabdd19cc45a5d2d8c88afbca3fe89a9003122
Tejas Dinkar <tejas@gja.in>
2014-12-04 15:15:30 +0530

    Added the ability for a line character at 84 characters

--- modules/clojure.el
+++ modules/clojure.el
@@ -1,12 +1,30 @@
-(nel:require-packages 'cider 'clj-refactor 'align-cljlet 'smartparens)
+(nel:require-packages 'cider 'clj-refactor 'align-cljlet 'smartparens 'fill-column-indicator)
 
 (setq cider-repl-history-file "~/.emacs.d/nrepl-history")
 (setq cider-auto-select-error-buffer t)
 (setq cider-repl-popup-stacktraces t)
 
+(defcustom clojure-column-line nil
+  "When non nil, puts a line at some character on clojure mode"
+  :type 'integer
+  :group 'clojure)
+
 (defun custom-cider-shortcuts ()
   (local-set-key (kbd "C-c ,") 'cider-test-run-tests)
   (local-set-key (kbd "C-c ,") 'cider-test-run-tests))
+
+(defun custom-turn-on-fci-mode ()
+  (when clojure-column-line
+    (setq fci-rule-column clojure-column-line)
+    (turn-on-fci-mode)))
+
+(defmacro clojure:save-before-running (function)
+  `(defadvice ,function (before save-first activate)
+     (save-buffer)))
+
+(defmacro clojure:load-before-running (function)
+  `(defadvice ,function (before save-first activate)
+     (cider-load-buffer)))
 
 (nel:run-after-initializing
   (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
@@ -15,14 +33,13 @@
   (add-hook 'clojure-mode-hook 'sp-use-paredit-bindings)
   (add-hook 'clojure-mode-hook (lambda () (cljr-add-keybindings-with-prefix "C-c C-m")))
   (add-hook 'clojure-mode-hook 'custom-cider-shortcuts)
+  (add-hook 'clojure-mode-hook 'custom-turn-on-fci-mode)
 
   (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
   (add-hook 'cider-repl-mode-hook 'show-paren-mode)
   (add-hook 'cider-repl-mode-hook 'sp-use-paredit-bindings)
 
-
-  (defadvice cider-test-run-tests (before save-first activate)
-    (save-buffer))
-
-  (defadvice cider-load-current-buffer (before save-first activate)
-    (save-buffer)))
+  (clojure:save-before-running cider-load-current-buffer)
+  (clojure:load-before-running cider-test-run-tests)
+  (clojure:load-before-running cider-test-rerun-tests)
+  (clojure:load-before-running cider-test-run-test))
