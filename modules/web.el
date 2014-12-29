217c31fe3797dfe805ff46b158306db9c01117c5
Tejas Dinkar <tejas@gja.in>
2014-12-29 14:00:24 +0530

    Added a jsx mode

--- modules/web.el
+++ modules/web.el
@@ -1,4 +1,4 @@
-(nel:require-packages 'web-mode 'scss-mode 'coffee-mode)
+(nel:require-packages 'web-mode 'scss-mode 'coffee-mode 'jsx-mode)
 
 (setq coffee-tab-width 2)
 (setq scss-compile-at-save nil)
