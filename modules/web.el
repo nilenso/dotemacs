f48f2448906d07dc8cd0f58773885e0a362e5bde
Tejas Dinkar <tejas@gja.in>
2014-12-30 16:29:21 +0530

    Added jshint

--- modules/web.el
+++ modules/web.el
@@ -1,4 +1,4 @@
-(nel:require-packages 'web-mode 'scss-mode 'coffee-mode 'jsx-mode)
+(nel:require-packages 'web-mode 'scss-mode 'coffee-mode 'jsx-mode 'jshint-mode)
 
 (setq coffee-tab-width 2)
 (setq scss-compile-at-save nil)
